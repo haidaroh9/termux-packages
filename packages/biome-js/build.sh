f4ac9fd652b5ded885e4454f580bbd14f5eb3155
haidaroh9 <haidaroh9@gmail.com>
2024-03-26 06:53:20 +0700

    Update build.sh

--- packages/biome-js/build.sh
+++ packages/biome-js/build.sh
@@ -7,10 +7,20 @@ TERMUX_PKG_VERSION="1.6.3"
 TERMUX_PKG_SRCURL=https://github.com/biomejs/biome/archive/refs/tags/cli/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a7172467bb0a15fa4c1138f5540493f8cb80297958b3d98ad80d18232cbb9537
 
+termux_step_pre_configure() {
+termux_setup_rust
+}
+
+
+termux_step_configure() {
+local env_host=$(printf $CARGO_TARGET_NAME | tr a-z A-Z | sed s/-/_/g)
+export CARGO_TARGET_${env_host}_RUSTFLAGS="-C strip=symbols"
+export JEMALLOC_SYS_WITH_LG_PAGE=16
+
+}
+
 termux_step_make() {
-	termux_setup_rust
-	export JEMALLOC_SYS_WITH_LG_PAGE=16 
-  RUSTFLAGS: "-C strip=symbols" cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 }
 
 termux_step_make_install() {
