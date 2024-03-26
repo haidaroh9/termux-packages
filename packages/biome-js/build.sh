12f761ed87c4dc02fc815bc507b43a7c498bbde5
haidaroh9 <haidaroh9@gmail.com>
2024-03-26 07:01:46 +0700

    Update build.sh

--- packages/biome-js/build.sh
+++ packages/biome-js/build.sh
@@ -9,6 +9,7 @@ TERMUX_PKG_SHA256=a7172467bb0a15fa4c1138f5540493f8cb80297958b3d98ad80d18232cbb95
 
 termux_step_pre_configure() {
 termux_setup_rust
+source "$HOME/.cargo/env"
 }
 
 
@@ -20,7 +21,7 @@ export JEMALLOC_SYS_WITH_LG_PAGE=16
 }
 
 termux_step_make() {
-        source "$HOME/.cargo/env"
+        
 	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 }
 
