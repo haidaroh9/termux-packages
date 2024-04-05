28130ec3a3ac3ff3fbc481cf9aa638fc5688ec8c
haidaroh9 <haidaroh9@gmail.com>
2024-04-05 20:47:31 +0700

    Update build.sh

--- packages/luarocks/build.sh
+++ packages/luarocks/build.sh
@@ -22,7 +22,8 @@ termux_step_configure() {
 	fi
 
 	./configure --prefix="$TERMUX_PREFIX" \
-		--with-lua-include="$TERMUX_PREFIX/include"
+		--with-lua-bin="$TERMUX_PREFIX/bin" \
+                --sysconfdir="$TERMUX_PREFIX/etc"
 }
 
 termux_step_post_make_install() {
